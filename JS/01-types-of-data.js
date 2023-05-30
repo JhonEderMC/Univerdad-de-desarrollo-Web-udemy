78710c8193e246affeedd58073138eb7d185772c
Jhon MC <jhoneder@hotmail.com>
2023-05-29 21:59:55 -0500

    basic data types in js

---
+++ JS/01-types-of-data.js
@@ -0,0 +1,14 @@
+/* types of data */
+
+// String
+const name = 'William';
+//number
+const number = 100;
+// Objet
+const objeto = {
+    name,
+    age: number
+};
+console.log('name:' +name , 'number: '+number);
+console.log('objeto:');
+console.table(objeto);
