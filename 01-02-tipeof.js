e002e5b876564d5eae0359f999f6409a7eef07d0
Jhon MC <jhoneder@hotmail.com>
2023-05-30 22:07:49 -0500

    type of variable

---
+++ 01-02-tipeof.js
@@ -0,0 +1,7 @@
+let name = 'david';
+let stringTypeOf = 'type of';
+console.log('name: '+name)
+console.log(`${stringTypeOf} name:`+ typeof(name));
+let number = 13.43;
+console.log('number:'+number);
+console.log(`${stringTypeOf} number:`+ typeof(number))
