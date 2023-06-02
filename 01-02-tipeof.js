7338300d5ee42f83d729039cf78099e07bf3e39f
Jhon MC <jhoneder@hotmail.com>
2023-06-01 21:30:55 -0500

    type of variable: boolean, function and symbol¿

--- 01-02-tipeof.js
+++ 01-02-tipeof.js
@@ -1,7 +1,25 @@
+//String
 let name = 'david';
 let stringTypeOf = 'type of';
-console.log('name: '+name)
+console.log('name: '+name);
 console.log(`${stringTypeOf} name:`+ typeof(name));
+
+//number
 let number = 13.43;
 console.log('number:'+number);
 console.log(`${stringTypeOf} number:`+ typeof(number))
+
+//boolean
+let flag = true;
+console.log('flag: '+ flag);
+console.log(`${stringTypeOf} flag: ${typeof(flag)}`);
+
+// function
+function myFunction(){};
+console.log('myFunction: '+myFunction);
+console.log(`${stringTypeOf} myFunction: ${myFunction}`);
+
+//Symbol
+let simbol = Symbol("my symbol");
+//console.log('symbol: '+simbol);
+console.log(`${stringTypeOf} symbol: ${typeof(simbol)}`);
