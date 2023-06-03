a3698382bd1f561854ac2ef9bf64fb808d0faced
Jhon MC <jhoneder@hotmail.com>
2023-06-03 18:01:58 -0500

    type of date null and undefined

--- 01-02-tipeof.js
+++ 01-02-tipeof.js
@@ -23,3 +23,23 @@ console.log(`${stringTypeOf} myFunction: ${myFunction}`);
 let simbol = Symbol("my symbol");
 //console.log('symbol: '+simbol);
 console.log(`${stringTypeOf} symbol: ${typeof(simbol)}`);
+
+// class
+class Person {
+    constructor(name, lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
+console.log('Class person: '+ Person);
+console.log(`${stringTypeOf} Person: ${typeof Person}`);
+
+//undefined
+let variableUndefined;
+console.log('variableUndefined: '+variableUndefined);
+console.log(`${stringTypeOf} variableUndefined: ${typeof variableUndefined}`);
+
+//null = ausencia de valor
+let variableNUll = null;
+console.log(`variableNull: ${variableNUll}`);
+console.log(`${stringTypeOf} variableNull: ${typeof variableNUll}`);
