3eed42eeffeb8b6c10d9b5fadb43b910f21ed941
Jhon MC <jhoneder@hotmail.com>
2023-06-12 18:32:29 -0500

    legal age

--- JS/01-08-ofAge.js
+++ JS/01-08-ofAge.js
@@ -1,7 +1,8 @@
 // You are of age
 const age = 17;
+const legalAge = 18;
 
-if (age >= 18) {
+if (age >= legalAge) {
     console.log('You are of age');
 } else {
     console.log('you are not of age ');
