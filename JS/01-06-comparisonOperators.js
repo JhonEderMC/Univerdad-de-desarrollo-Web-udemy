66c5eba21506a851ef34d9f5128c6e52d920e145
Jhon MC <jhoneder@hotmail.com>
2023-06-11 22:00:59 -0500

    operators of comparison

---
+++ JS/01-06-comparisonOperators.js
@@ -0,0 +1,10 @@
+let a = 3, b = 2 , c = "3";
+
+console.log(`a : ${a}`);
+console.log(`b : ${b}`);
+console.log(`c : "${c}"`);
+
+console.log(`a == b : ${a == b}`);
+console.log(`a == c : ${ a == c}`);
+console.log(`a === b : ${a === b}`);
+console.log(`a === c : ${ a === c}`);
