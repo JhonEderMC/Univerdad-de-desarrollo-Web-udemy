6864a39bdbe0fbf6a4a18a34e533d0b0cf362a61
Jhon MC <jhoneder@hotmail.com>
2023-06-11 22:04:07 -0500

    add comments

--- JS/01-06-comparisonOperators.js
+++ JS/01-06-comparisonOperators.js
@@ -4,7 +4,7 @@ console.log(`a : ${a}`);
 console.log(`b : ${b}`);
 console.log(`c : "${c}"`);
 
-console.log(`a == b : ${a == b}`);
-console.log(`a == c : ${ a == c}`);
+console.log(`a == b : ${a == b}`); 
+console.log(`a == c : ${ a == c}`); // the value is checked regardless of the type
 console.log(`a === b : ${a === b}`);
-console.log(`a === c : ${ a === c}`);
+console.log(`a === c : ${ a === c}`); // the value is checked importing the type
