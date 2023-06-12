01509ee44e6ac1574a59a70a5fde66d0953e496e
Jhon MC <jhoneder@hotmail.com>
2023-06-11 21:43:01 -0500

    assigment operators

---
+++ JS/01-05-assigmentOperators.js
@@ -0,0 +1,21 @@
+// assigment 
+let a = 0;
+console.log(`a: ${a}`);
+
+// assigmennt increment or decrement
+a +=2; //a = a +2;
+console.log(`a += 1 : ${a}`);
+a -=2; // a = a -2
+console.log(`a -= 1 : ${a}`);
+
+//Asigment multiplication
+a = 2; 
+console.log(`a: {a}`);
+a *= 2; // a = a * 2;
+console.log(`a *=2 :${a}`);
+a /=2; // a = a / 2;
+console.log(`a /=2 : ${a}`);
+/*
+%=
+**=
+*/
