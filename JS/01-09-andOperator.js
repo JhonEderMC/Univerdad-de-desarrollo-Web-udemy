d051b81766ea58e7bc5488db8373b611ab2d6469
Jhon MC <jhoneder@hotmail.com>
2023-06-12 18:56:52 -0500

    and operator with conditions

---
+++ JS/01-09-andOperator.js
@@ -0,0 +1,9 @@
+// Range o value
+const a = 7;
+const minValue = 0, maxValue = 10;
+
+if ( a >= minValue && a <= maxValue) {
+     console.log(`${a} is between ${minValue} and ${maxValue} `);
+} else {
+    console.log(`${a} is not between ${minValue} and ${maxValue}`);
+}
