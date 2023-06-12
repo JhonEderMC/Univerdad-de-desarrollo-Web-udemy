3c677e7e23029bbcfb12ff76832a14aa3c8736e3
Jhon MC <jhoneder@hotmail.com>
2023-06-12 10:19:58 -0500

    comparison operators

--- JS/01-06-matematicsOperators.js
+++ JS/01-06-matematicsOperators.js
@@ -12,8 +12,29 @@ console.log(`a === b : ${a === b}`);
 console.log(`a === c : ${ a === c}`); // the value is checked importing the type
 
 // Different
+printVariables();
 console.log('Different:')
 console.log(`a != b : ${a != b}`); 
 console.log(`a != c : ${ a != c}`); // the value is checked regardless of the type
 console.log(`a !== b : ${a !== b}`);
 console.log(`a !== c : ${ a !== c}`); // the value is checked importing the type
+
+// Comparison
+printVariables();
+console.log('Comparison:')
+console.log(`a > b: ${a > b} `);
+console.log(`a >= b: ${a >= b} `);
+console.log(`a < b: ${a < b} `);
+console.log(`a <= b: ${a <= b} `);
+console.log(`a > c: ${a > c} `);
+console.log(`a >= c: ${a >= c} `);
+console.log(`a >= c: ${a >= c} `);
+
+
+
+
+function printVariables() {
+    console.log(`a : ${a}`);
+    console.log(`b : ${b}`);
+    console.log(`c : "${c}"`);
+}
