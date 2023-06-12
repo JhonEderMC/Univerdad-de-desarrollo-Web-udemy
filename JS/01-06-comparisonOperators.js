b19cfd52d424c90032010410f7cfb78d3fdad304
Jhon MC <jhoneder@hotmail.com>
2023-06-12 09:54:56 -0500

    different operators

--- JS/01-06-comparisonOperators.js
+++ JS/01-06-comparisonOperators.js
@@ -1,10 +1,19 @@
 let a = 3, b = 2 , c = "3";
 
+//Equality
 console.log(`a : ${a}`);
 console.log(`b : ${b}`);
 console.log(`c : "${c}"`);
+console.log('Equality: ')
 
 console.log(`a == b : ${a == b}`); 
 console.log(`a == c : ${ a == c}`); // the value is checked regardless of the type
 console.log(`a === b : ${a === b}`);
 console.log(`a === c : ${ a === c}`); // the value is checked importing the type
+
+// Different
+console.log('Different:')
+console.log(`a != b : ${a != b}`); 
+console.log(`a != c : ${ a != c}`); // the value is checked regardless of the type
+console.log(`a !== b : ${a !== b}`);
+console.log(`a !== c : ${ a !== c}`); // the value is checked importing the type
