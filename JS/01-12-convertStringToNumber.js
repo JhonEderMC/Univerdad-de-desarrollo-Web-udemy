9cd2f30b6efa1c2eeb4a5d0156454c5f83c733e0
Jhon MC <jhoneder@hotmail.com>
2023-06-12 21:39:30 -0500

    string to number

---
+++ JS/01-12-convertStringToNumber.js
@@ -0,0 +1,7 @@
+/* String to Number */
+const stringNumber = "07";
+console.log(`stringNumber is type of: ${typeof stringNumber}`);
+//String to number
+const number = Number(stringNumber);
+
+console.log(`number is type of: ${typeof number}`);
