894f4051b0c9ae14fa2f93740095108afbb9785e
Jhon MC <jhoneder@hotmail.com>
2023-06-12 19:50:32 -0500

    ternary expresion

---
+++ JS/01-11-ternariExpresion.js
@@ -0,0 +1,10 @@
+/* Ternari operator
+    condition ? true : false;
+*/
+
+true ? print('true condition') : print('false condition');
+false ? print('true condition') : print('false condition');
+
+function print(text) {
+    console.log(text);
+}
