b9a1e821064ad2656f5665d15df09b9a67dbf062
Jhon MC <jhoneder@hotmail.com>
2023-06-12 19:59:23 -0500

    ternary expresion

--- JS/01-11-ternariExpresion.js
+++ JS/01-11-ternariExpresion.js
@@ -2,8 +2,12 @@
     condition ? true : false;
 */
 
+3 > 2 ? print('true condition') : print('false condition');
 true ? print('true condition') : print('false condition');
 false ? print('true condition') : print('false condition');
+
+let value = 3 < 2 ? "true" : "false";
+console.log(value);
 
 function print(text) {
     console.log(text);
