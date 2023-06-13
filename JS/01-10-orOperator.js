cf0c09dfa8832137c1545e834f393ec9cb6de03f
Jhon MC <jhoneder@hotmail.com>
2023-06-12 19:22:06 -0500

    Or operator in JS

---
+++ JS/01-10-orOperator.js
@@ -0,0 +1,11 @@
+/*
+ a parent may attend his or her child's game if he or her has a vacation or day off.
+*/
+
+const isVacation = false, isDayoff = true;
+
+if ( isVacation || isDayoff) {
+    console.log('A parent may attend his o her childs game');
+}else {
+    console.log('A parent does not attend his or her childs game');
+}
