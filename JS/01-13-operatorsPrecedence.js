122d63a8a11297bd93b2deb3ae37ac414c2227ef
Jhon MC <jhoneder@hotmail.com>
2023-06-13 20:23:49 -0500

    procedence operators. Some examples about procedence of operators

---
+++ JS/01-13-operatorsPrecedence.js
@@ -0,0 +1,16 @@
+/* Operators precedence */
+let x = 5;
+let y = 10;
+
+console.log(`x: ${x}`);
+console.log(`y: ${y}`);
+
+let z = ++x + y--;
+console.log(`z = ++x + y--: ${z}`);
+console.log(`x: ${x}`);
+console.log(`y: ${y}`);
+
+let sum1 = 4 + 5*6/3;
+console.log(`4 + 5*6/3 = ${sum1}`);
+let sum2 = (4+5)*6/3;
+console.log(`(4+5)*6/3 = ${sum2}`)
