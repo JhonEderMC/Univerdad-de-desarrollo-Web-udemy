a3832810631be330b2c9f8216876ee988b7666cc
Jhon MC <jhoneder@hotmail.com>
2023-06-18 09:29:33 -0500

    Example if else operator

--- JS/02-01-ifElse.js
+++ JS/02-01-ifElse.js
@@ -6,3 +6,21 @@ if(condition) {
 }else {
     console.log('if the condition is false');
 }
+
+//Example
+let number =  7;
+console.log(`The nunmber is: ${number}`);
+
+if(number == 1) {
+    console.log('The number es 1');
+} else if(number == 2) {
+    console.log('The number es 2');
+} else if(number == 3) {
+    console.log('The number es 3');
+} else if(number == 4) {
+    console.log('The number es 4');
+} else if(number == 5) {
+    console.log('The number es 5');
+} else {
+    console.log('UnKown number');
+}
