edcd6ea45c3f9220010f634b9cae290a40271249
Jhon MC <jhoneder@hotmail.com>
2023-06-18 20:49:09 -0500

    structure switch

---
+++ JS/02-02-switch.js
@@ -0,0 +1,31 @@
+/* switch */
+
+let number = 7;
+
+switch (number) {
+    case 1:
+        console.log('The number is 1');        
+        break;
+    case 2:
+        console.log('The number is 2');
+        break;
+    case 3:
+        console.log('The number is 3');
+        break;
+    case 4:
+        console.log('The number is 4'); 
+        break;
+    case 5:
+        console.log('The number is 5');
+         break;
+    case 6:
+        console.log('The number is 6'); 
+        break;
+    case 7:
+        console.log('The number is 7'); 
+        break;              
+    default:
+        case 2:
+        console.log('Unkown Number');
+        break;
+}
