a9be6edc172d9c61daabbf0c71829bad10147228
Jhon MC <jhoneder@hotmail.com>
2023-06-19 21:59:58 -0500

    word continue

---
+++ JS/05-01-continue.js
@@ -0,0 +1,8 @@
+/* word continue -> go to the next execution cycle  */
+
+for (let index = 0; index <= 10; index++) {
+    if(index %2 == 0){
+        continue;             
+    }
+    console.log(index);    
+}
