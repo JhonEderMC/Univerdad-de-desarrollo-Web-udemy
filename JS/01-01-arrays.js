c012be576707c9c80ff95d67ecaebdedfdfe4967
Jhon MC <jhoneder@hotmail.com>
2023-06-20 22:20:45 -0500

    arrays and how iterate it

--- JS/01-01-arrays.js
+++ JS/01-01-arrays.js
@@ -7,8 +7,16 @@ console.table(cars);
  console.log(`cars[3]: ${cars[3]}`);
 
  //For regular
- console.log('regular for:')
+ console.log('Regular for:')
  for (let index = 0; index <cars.length; index++) {
     const element = array[index];
     console.log(element);    
+ }
+
+ //for in
+ for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
  }
