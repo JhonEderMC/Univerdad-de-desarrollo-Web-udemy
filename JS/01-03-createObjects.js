8f3f80f9645cf0aada844bd68e4929b085427800
Jhon MC <jhoneder@hotmail.com>
2023-07-03 21:46:15 -0500

    create object using function

--- JS/01-03-createObjects.js
+++ JS/01-03-createObjects.js
@@ -13,3 +13,17 @@ person2.age = 32;
 
 console.log('Person1: ', person1);
 console.log('Person2: ', person2);
+
+//Using function
+function car(make, model, year ) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+}
+//create object
+const car1 = new car('American Eagle', 'MSI', '1992');
+console.log('\nCreate object car using a function: ', car1);
+
+//Using Object.create -> create un nuew object base in original object
+const person3 = Object.create(person1);
+console.log('Using Object.create(person1): ',person3);
