1639455b9543ca8716e5bb65dd48f726884ead19
Jhon MC <jhoneder@hotmail.com>
2023-07-10 18:14:23 -0500

    class in js

---
+++ 01-01-class.js
@@ -0,0 +1,13 @@
+//First create class person
+class Person {   
+
+    constructor(name, lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
+
+const henry = new Person('Henry', 'Ateortua');
+const sobeida = new Person('Sobieda', 'Murillo');
+console.log(henry);
+console.log(sobeida);
