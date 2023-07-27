5cfbe4033e49c5c35095dae08ae80b5a1d76fedf
Jhon MC <jhoneder@hotmail.com>
2023-07-26 22:14:26 -0500

    class client

--- JS/inherit-exercise.js
+++ JS/inherit-exercise.js
@@ -67,3 +67,32 @@ class Employee extends Person {
 
 const employee = new Employee('Sara', 'Perez', 33, 3000);
 console.log(employee.toString());
+
+ class Client extends Person {
+
+    static countClient = 0;
+
+    constructor(name, lastName, age, registerDate) {
+        super(name, lastName, age);
+        this._registerDate = registerDate;
+        this._idClient = ++Client.countClient;
+    }
+
+    get idClient() {
+        return this._idClient;
+    }
+    get registerDate() {
+        return this._registerDate;
+    }
+    set registerDate(registerDate) {
+        this._registerDate = registerDate;
+    }
+
+    toString() {
+        return `${super.toString()} idClient: ${this.idClient} registerDate: ${this.registerDate}`;
+    }
+
+ }
+
+ const client = new Client('Diego', 'Perez', 14, new Date());
+ console.log(client.toString());
