ad585319d4133160f577cbc0dc01b1d974b5b4bd
Jhon MC <jhoneder@hotmail.com>
2023-08-28 22:24:53 -0500

    types of functions

---
+++ JS/01-arrowFuntions.js
@@ -0,0 +1,16 @@
+//Normal Functions
+function myNormalFunctions() {
+    console.log('Welcome to my normal function');
+}
+
+myNormalFunctions();
+
+//Anonymous function
+let anonymousFunction = function () {
+    console.log('Welcome to anonymous function');
+}
+anonymousFunction();
+
+//Arrow function -> That's anonymous function
+let arrowFunction = () => {console.log('Welcome to arrow function')}
+arrowFunction();
