20174b1279574bb63da4c62995db325749cc0b57
Jhon MC <jhoneder@hotmail.com>
2023-08-29 21:42:11 -0500

    arrow functions

--- JS/01-arrowFuntions.js
+++ JS/01-arrowFuntions.js
@@ -12,5 +12,23 @@ let anonymousFunction = function () {
 anonymousFunction();
 
 //Arrow function -> That's anonymous function
-let arrowFunction = () => {console.log('Welcome to arrow function')}
+let arrowFunction = () => console.log('Welcome to arrow function')
 arrowFunction();
+
+const hello = () => 'hello';
+console.log(hello());
+
+//return object
+const objectName = () => ({name: 'arrow function'});
+console.log(objectName());
+
+//Functions with params
+const functionWitParams = (message) => console.log(message);
+functionWitParams('params');
+
+//Functions with params. If is one param parentesis they are not necessary
+const functionWithParamsOneParam = message => console.log(message);
+functionWithParamsOneParam('one param');
+
+const sum = (...args)=> args.reduce((prev,curr) => prev + curr);
+console.log(sum(1,2,3,4,5,6,7,8,9));
