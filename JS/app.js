2de4425463c38e7532c0be00ab7de64909addd97
Jhon MC <jhoneder@hotmail.com>
2023-10-16 22:18:04 -0500

    add html form and managment button click in app.js

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,7 @@
 function sum() {
     console.log('hi');
+    const form = document.forms['form'];
+
+    console.log('number1: ',form['number1'].value);
+    console.log('number2: ',form['number2'].value);
 }
