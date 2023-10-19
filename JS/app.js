2d7d499eedbb355dc10d875bb96eb49369fd69db
Jhon MC <jhoneder@hotmail.com>
2023-10-18 21:57:02 -0500

    add logic for show resutl after click sum button

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,11 @@
 function sum() {
     console.log('hi');
     const form = document.forms['form'];
+    //const sum = Number(form['number1'].value) + Number(form['number2'].value);
+    const sum = +form['number1'].value + +form['number2'].value;
 
     console.log('number1: ',form['number1'].value);
     console.log('number2: ',form['number2'].value);
+    const result = document.getElementById('result');
+    result.innerHTML = `El resultado es: ${sum}`;
 }
