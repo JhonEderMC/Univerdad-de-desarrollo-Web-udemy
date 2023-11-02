7f9b69191a38e67b4349f87713f11256e388c1a0
Jhon MC <jhoneder@hotmail.com>
2023-11-01 22:26:58 -0500

    add format hour and togle css class boder

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -2,12 +2,20 @@ const showClock = () => {
     const clock = new Date();
 
     const htmlHour = document.getElementById('hour');
-    htmlHour.innerHTML = `${clock.getHours()} ${clock.getMinutes()} ${clock.getSeconds()}`;  
+    htmlHour.innerHTML = `${formatHour(clock.getHours())} ${formatHour(clock.getMinutes())} ${formatHour(clock.getSeconds())}`;  
 
     const htmlDate = document.getElementById('date');
     htmlDate.innerHTML = `${clock.toDateString()}`
     
     console.log(clock.toDateString())
+
+    console.log(document.getElementById('container').classList);
+    document.getElementById('container').classList.toggle('animate');
+    
+}
+
+const formatHour = (hour) => {
+    return hour < 10 ? `0${hour}`: `${hour}`;
 }
 
 
