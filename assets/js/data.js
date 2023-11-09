b79940d8a3074949e931261dfb1457172a42aef2
Jhon MC <jhoneder@hotmail.com>
2023-11-08 22:02:48 -0500

    add class Data pojo

---
+++ assets/js/data.js
@@ -0,0 +1,25 @@
+class Data {
+
+    constructor(value, description) {
+        this._value = value;
+        this._description = description;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    set value(value) {
+        this._value = value;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    set description(description) {
+        this._description = description;
+    }
+
+
+}
