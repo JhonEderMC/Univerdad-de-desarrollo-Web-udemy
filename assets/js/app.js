1e0e9e712ae2365aa3cc145d31739afaac80459a
Jhon MC <jhoneder@hotmail.com>
2023-11-08 22:24:58 -0500

    create income and expenditures arrays

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+
+const income = [
+    new Income('Salary', 2100),
+    new Income('Sell car', 1500)
+];
+
+const expenditures = [
+    new Expenditure('Rent house', 900),
+    new Expenditure('Clothes', 400)
+];
