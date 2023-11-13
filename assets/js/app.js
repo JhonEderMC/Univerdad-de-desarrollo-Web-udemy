ee8338255095c16d94c6d735010f07dcc10d3262
Jhon MC <jhoneder@hotmail.com>
2023-11-13 10:27:06 -0500

    add logic to calculate budget and total incomes and expenditures to show it en html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,36 @@
 
 const income = [
-    new Income('Salary', 2100),
-    new Income('Sell car', 1500)
+    new Income(2100, 'Salary' ),
+    new Income(1500, 'Sell car')
 ];
 
 const expenditures = [
-    new Expenditure('Rent house', 900),
-    new Expenditure('Clothes', 400)
+    new Expenditure(900, 'Rent house'),
+    new Expenditure(400, 'Clothes')
 ];
+
+
+const loadApp = () => {
+    loadHead();
+}
+
+
+const loadHead = () => {
+    const totalIncome = income.reduce((prev, curr) => prev.value + curr.value);
+    const totalExpenditure = expenditures.reduce((prev, curr) => prev.value + curr.value);
+    const budget = totalIncome - totalExpenditure;
+    const expenditurePorcentage =(((totalExpenditure)/budget)*100)    
+
+    document.getElementById('income').innerHTML = `+ ${formatCurrency(totalIncome)}`;
+    document.getElementById('expenditure').innerHTML = `- ${formatCurrency(totalExpenditure)}` ;
+    document.getElementById('budget').innerHTML = `${formatCurrency(budget)}`;
+    document.getElementById('expenditure_porcentage').innerHTML = `${formatPercent(expenditurePorcentage, 2)}%`
+
+    console.log(totalIncome);
+    console.log(totalExpenditure);
+    console.log(expenditurePorcentage);
+
+}
+
+const formatCurrency = (number) => Number(number).toLocaleString('en-US', {style: 'currency', currency: 'USD', minimumFractionDigits:2});
+const formatPercent = (number, fixed) => Number(number).toFixed(fixed);
