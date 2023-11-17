0652cea769711108a7799fa0c219ab73cf625d57
Jhon MC <jhoneder@hotmail.com>
2023-11-16 22:23:51 -0500

    add logic for create html from income and expenditure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,10 @@
+let totalIncome = '';
+let totalExpenditure = '';
+let budget = '';
+let expenditurePorcentage ='';
+
+
+
 
 const income = [
     new Income(2100, 'Salary' ),
@@ -12,14 +19,16 @@ const expenditures = [
 
 const loadApp = () => {
     loadHead();
+    loadIncome();
+    loadExpenditure();
 }
 
 
 const loadHead = () => {
-    const totalIncome = income.reduce((prev, curr) => prev.value + curr.value);
-    const totalExpenditure = expenditures.reduce((prev, curr) => prev.value + curr.value);
-    const budget = totalIncome - totalExpenditure;
-    const expenditurePorcentage =(((totalExpenditure)/budget)*100)    
+    totalIncome = income.reduce((prev, curr) => prev.value + curr.value);
+    totalExpenditure = expenditures.reduce((prev, curr) => prev.value + curr.value);
+    budget = totalIncome - totalExpenditure;
+    expenditurePorcentage =(((totalExpenditure)/budget)*100)    
 
     document.getElementById('income').innerHTML = `+ ${formatCurrency(totalIncome)}`;
     document.getElementById('expenditure').innerHTML = `- ${formatCurrency(totalExpenditure)}` ;
@@ -34,3 +43,47 @@ const loadHead = () => {
 
 const formatCurrency = (number) => Number(number).toLocaleString('en-US', {style: 'currency', currency: 'USD', minimumFractionDigits:2});
 const formatPercent = (number, fixed) => Number(number).toFixed(fixed);
+
+const loadIncome = () => {
+    let incomeListHTML = '';
+    income.forEach((incm) => incomeListHTML += createHTMLincome(incm));
+    document.getElementById('income-list').innerHTML = incomeListHTML;
+}
+
+const loadExpenditure = () => {
+    let expenditureListHTML = '';
+    expenditures.forEach((exp) => expenditureListHTML += createExpenditureHTML(exp));
+    document.getElementById('expenditure-list').innerHTML = expenditureListHTML;
+}
+
+function createHTMLincome(income) {
+    return `   <div class="element clearEstyles">
+                    <div class="element_description">${income.description}</div>
+                    <div class="right clearEstyles">
+                        <div class="element_value">${formatCurrency(income.value)}</div>
+                        <div class="element_delete">
+                            <button class="element_delete--btn">
+                                <ion-icon name="close-circle-outline"></ion-icon>
+                            </button>
+                        </div>
+                    </div>
+                </div>`
+}
+
+function createExpenditureHTML(expenditure) {
+    const percent = (expenditure.value/budget)*100;
+    return `    <div class="element clearEstyles">
+                    <div class="element_description">${expenditure.description}</div>
+                    <div class="right clearEstyles">
+                        <div class="element_value">${formatCurrency(expenditure.value)}</div>
+                        <div class="element_porcentage">${formatPercent(percent)}%</div>
+                        <div class="element_delete">
+                            <button class="element_delete--btn">
+                                <ion-icon name="close-circle-outline"></ion-icon>
+                            </button>
+                        </div>
+                    </div>
+                </div>`
+}
+
+
